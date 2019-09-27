65b2f6bdada95d36d3965d5d5023418b68166926
JoseArela <55859133+JoseArela@users.noreply.github.com>
2019-09-26 19:30:28 -0500

    Update EchoBot.cs

--- BotFisUnica-src/Bots/EchoBot.cs
+++ BotFisUnica-src/Bots/EchoBot.cs
@@ -22,7 +22,7 @@ namespace Microsoft.BotBuilderSamples.Bots
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
                 {                    
-                    aguarde turnContext.SendActivityAsync(MessageFactory.Texto($"Hola y Benvenido!"), cancellationToken);
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Hola y Benvenido!"), cancellationToken);
                 }
             }
         }
